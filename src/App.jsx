cc4bba06e87eac400cb825600321e735ac8bebcd
julio candeia <juliocandeia594@gmail.com>
2025-06-22 11:26:55 -0300

    chore: created and install tools to project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center flex-col gap-4">
+      <h1 className="text-4xl font-bold">Opex está no ar!</h1>
+      <h2 className="bg-blue-500 text-white p-4 rounded-md">teste</h2>
+    </div>
+  )
+}
